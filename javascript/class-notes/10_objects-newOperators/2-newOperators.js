d31e21879df8a9c18980d5d5e83f9736e9337604
brs14shn <brsshn292929@gmail.com>
2022-05-26 22:36:15 +0300

    new opt v3

--- javascript/class-notes/10_objects-newOperators/2-newOperators.js
+++ javascript/class-notes/10_objects-newOperators/2-newOperators.js
@@ -39,3 +39,46 @@ console.log(car1,car2);
 const {name:c2name,model:c2model}=car2;
 
 console.log(c2name,c2model);
+
+
+//? JSON => javascript Object Notation
+const kisiler = [
+  {
+    name: 'Ahmet',
+    surname: 'Can',
+    job: 'developer',
+    age: 30,
+  },
+  {
+    name: 'Mehmet',
+    surname: 'Baki',
+    job: 'tester',
+    age: 35,
+  },
+  {
+    name: 'Nur',
+    surname: 'Ersan',
+    job: 'team lead',
+    age: 40,
+  }];
+
+  kisiler.forEach((kisi)=>{
+    console.log("****************")
+    const {name,job,surname,age}=kisi
+    console.log("NAME:",kisi.name);
+    console.log("SURNAME:",kisi.surname);
+    console.log("AGE:",kisi.age);
+  })
+
+
+
+//* function
+function getInfo() {
+  return {
+    id: 101,
+    name: "Apple",
+    price: 20000,
+  };
+}
+const { id, name, price } = getInfo();
+console.log(id, price);
