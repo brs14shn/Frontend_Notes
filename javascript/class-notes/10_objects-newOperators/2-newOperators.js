c807d56a91eb78132b545f314a435130ac434163
brs14shn <brsshn292929@gmail.com>
2022-05-26 22:03:26 +0300

    new operatos file was updated

---
+++ javascript/class-notes/10_objects-newOperators/2-newOperators.js
@@ -0,0 +1,13 @@
+//* ======================================================
+//*    NEW GENERATION OPERATORS: DESTRUCTURING (OBJECT)
+//* ======================================================
+
+const car = {
+  name: 'BMW',
+  model: 1990,
+  engine: 1.6,
+};
+
+//* 1.YONTEM (Classical)
+console.log(car.model);
+console.log(car['name']);
