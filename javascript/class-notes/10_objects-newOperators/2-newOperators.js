d55cf65d19a2e76529400a965cbd67bf275b1016
brs14shn <brsshn292929@gmail.com>
2022-05-26 22:26:58 +0300

    new opt v2

--- javascript/class-notes/10_objects-newOperators/2-newOperators.js
+++ javascript/class-notes/10_objects-newOperators/2-newOperators.js
@@ -11,3 +11,31 @@ const car = {
 //* 1.YONTEM (Classical)
 console.log(car.model);
 console.log(car['name']);
+
+
+//2.yöntem==>destructuring
+
+
+const {name,model}=car;
+
+console.log(name);
+
+// //* EXAMPLE: NESTED
+const cars = {
+  car1: {
+    name1: "BMW",
+    model1: 1990,
+    engine: 1.6,
+  },
+  car2: {
+    name: "Mercedes",
+    model: 2022,
+    engine: 2.0,
+  },
+};
+const {car1,car2}=cars;
+console.log(car1,car2);
+
+const {name:c2name,model:c2model}=car2;
+
+console.log(c2name,c2model);
