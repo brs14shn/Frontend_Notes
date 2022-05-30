85d3d93cdfcce34e38d17b8bc7edebae84152bec
brs14shn <brsshn292929@gmail.com>
2022-05-30 22:00:47 +0300

    addBtn and deleteBtn onclick was added

--- javascript/class-notes/11_dom_examples/app.js
+++ javascript/class-notes/11_dom_examples/app.js
@@ -1,7 +1,7 @@
 console.log('**** app.js *******');
 
 
-const inputLang=document.querySelector('.languages')
+const inputLang=document.querySelector('.languages');
 const addBtn=document.getElementById('add');
 const deleteBtn=document.getElementById('delete')
 
@@ -15,9 +15,10 @@ langSection.appendChild(ulCreate);
 console.log(langSection.children[0]);
 
 
+
 //? SELECTORS
 //************************************************* */
-console.log(dilSection.children[0]);
+/*console.log(dilSection.children[0]);
 console.log(dilSection.parentNode.parentNode);
 
 // const h1 = ul.closest('.container').firstChild;
@@ -27,7 +28,7 @@ h1.style.color = 'red';
 
 //?container class ı içerisindeki btn class'ına ait olan elementleri arar.
 const buttons = ul.closest('.container').querySelectorAll('.btn');
-console.log(buttons);
+console.log(buttons);*/
 //************************************************* */
 
 
@@ -37,11 +38,54 @@ addBtn.addEventListener('click', () => {
 
     if (!inputLang.value){
         alert("Please enter a add languages");
-    }else {
-        const li = document.createElement("li");
-        ulCreate.appendChild(li)
-        
-    
-        
-
     }
+    else {
+        ulCreate.innerHTML+=`<li>${inputLang.value}<li>`;
+        inputLang.value="";
+        JavascriptKontrol();
+    }
+        
+})
+
+
+const JavascriptKontrol=()=>{
+    document.querySelectorAll("ul li").forEach((dil)=>{
+       const kucukHarf=dil.textContent.toLocaleLowerCase();
+        if(kucukHarf==="javascript"){
+            //dil.className='red';
+            dil.setAttribute('class','red')
+
+        } 
+    })
+}
+
+//! delete btn fonksiyonu
+deleteBtn.onclick=function(){
+    ulCreate.childElementCount>0
+    ? ulCreate.removeChild(ulCreate.lastElementChild)
+    :alert('Silinecek eleman kalmadı')
+}
+
+
+inputLang.addEventListener('keydown',(e)=>{
+if(e.code==='Enter'){
+addBtn.click()
+
+}
+if(e.keyCode===108){
+    deleteBtn.onclick();
+}
+
+})
+
+//inputLang.focus() or
+
+window.onload=()=>{
+    JavascriptKontrol();
+    inputLang.focus();
+    
+}
+
+
+
+
