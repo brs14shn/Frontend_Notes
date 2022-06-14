e19c3934c89b7ee03fba877e1dd078cece3151bd
brs14shn <brsshn292929@gmail.com>
2022-06-14 20:46:57 +0300

    timeout

--- javascript/class-notes/15_async1/1-timeout_Interval.js
+++ javascript/class-notes/15_async1/1-timeout_Interval.js
@@ -2,7 +2,9 @@
 //*              1- Senkron-Asenkron Programlama
 //* ================================================================
 
-//! Javascript, bir single-threaded ve Asenkron Programlama dilidir.
+//! Javascript, bir single-threaded (birim zamanda -1- tane iş parçacığı <<kolumuz bir process ise parmaklarımız thread (iş parçacığı)'dir>> çalıştırıyor. Ancak Java, C++, Python'da multi-threaded aktif hale getirilebiliyor) ve Asenkron Programlama dilidir.
+
+//! CPU birden fazla iş yaparken aslında zamanı küçük dilimlere böldüğünde milisaniye seviyesinde. belli bir zaman aralığında birden fazla iş yapıyormuş gibi geliyor.
 
 //? Asenkron Programlama
 //? ---------------------------------------------------------------
@@ -19,4 +21,27 @@
 //* Senkron
 //* ---------------------------------------------------------------
 
-console.log('TIMEOUT');
+const bekle=(ms)=>{
+
+    //! blocking-code
+    const start=new Date().getTime(); //1655227837903
+    while(new Date().getTime()<start+ms){
+        ;
+    }
+}
+
+console.log("hello")
+//alert(  "ITF")  //! blocking code==> KODU KESER.TAMAMLANMADAN DİĞERİ GEÇM
+console.time("timer");
+bekle(3000)
+console.timeEnd("timer")
+console.log("FS11"); 
+
+
+//* Asenkron (setTimeout)
+
+setTimeout(()=>{ //! non-blocking code
+    console.log("Nasılsın");
+},1000)  //setTimeout(function, milliseconds);-bir defa yürütülüyor
+
+console.log("İyiyim");
