35887d3af3e41ac10697366fec2c3719d0235278
brs14shn <brsshn292929@gmail.com>
2022-06-15 20:30:53 +0300

    async-2 was added

---
+++ javascript/class-notes/js16_async2/async-await/4-async-await.js
@@ -0,0 +1,44 @@
+//*===============================================================
+//*                   4- ASYNC-AWAIT
+//*===============================================================
+//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
+//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
+//? Bu baglamda sentetik seker benzetmesi yapilabilir.
+
+//* Bir fonskiyonu asyn hale getirmek icin fonksiyon keyword'nun onune
+//* asyn keyword'u eklenir.
+
+//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
+//* beklenmesi saglanir.
+
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
+//* kod yazmayı mumkun kilar.
+
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
+//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
+//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+
+fetch('https://api.github.com/users')
+  .then((res) => {
+    //! error handling
+    if (!res.ok) {
+      throw new Error(`Something went wrong: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => updateDom(data))
+  .catch((err) => console.log(err));
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector('.users');
+
+  data.forEach((user) => {
+    const { login, avatar_url, html_url } = user;
+    userDiv.innerHTML += `
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />;
+    <h3>HTML_URL :${html_url}</h3>
+  `;
+  });
+};
