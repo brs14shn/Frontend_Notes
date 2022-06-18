662609f36ecf428098af45199c1325001f00b7e6
brs14shn <brsshn292929@gmail.com>
2022-06-18 13:35:30 +0300

    classses upt

--- javascript/class-notes/js18_oop1/3-classses.js
+++ javascript/class-notes/js18_oop1/3-classses.js
@@ -1,17 +1,34 @@
+//* ======================================================
+//*     OOP - Inheritance and Polymorphism(ES6)
+//* ======================================================
 
+//? Bir parent class'in degisken ve fonksiyonelliği extends keyword'u ile
+//? child class'a gecmektedir.(INHERITANCE)
 
+//* Polymorphism, bir degisken, fonksiyon veya nesnenin çoklu sekiller
+//* alabilmesini tanimlayan bir nesne-yonelimli programlama teknigidir.
+//* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar ile bilinir.
 
-
-
-
-class Book{
-    constructor(title,author,year){
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.getSummary = function () {
-      return `${this.title} was written by ${this.author} in ${this.year} `;
-    };
-
+class Book {
+    constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+      // this.getTitle = function () {
+      //   return this.title;
+      // };
     }
-}
+    //! bu kısımda yazılan fonksiyonlar prototype alanında bulunur
+    getSummary() {
+      return `${this.title} was written by ${this.author} in ${this.year} `;
+    }
+  }
+  
+  //? instance
+  const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
+  const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
+  
+  console.log(book1);
+
+  //? sub-Class tanımlanması
+  class Magazine extends Book()
