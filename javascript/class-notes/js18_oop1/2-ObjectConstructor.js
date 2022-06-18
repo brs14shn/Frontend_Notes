9a469735ef97053222a72fdbdb5d723c209cf4fc
brs14shn <brsshn292929@gmail.com>
2022-06-18 12:38:41 +0300

    proto

--- javascript/class-notes/js18_oop1/2-ObjectConstructor.js
+++ javascript/class-notes/js18_oop1/2-ObjectConstructor.js
@@ -8,18 +8,6 @@
 //* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
 //* Ornegin Date and Array nesneleri Object.prototype'dan miras almaktadir.
 
-
-
-//* ======================================================
-//*             OOP - Object Constructor (ES5)
-//* ======================================================
-
-//* Javascript, prototype-temelli bir dildir.
-//* Butun JavaScript nesneleri (objects) ozellikleri (properties)
-//* ve metotlari bir prototipten miras olarak almaktadir.
-//* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
-//* Ornegin Date and Array nesneleri Object.prototype'dan miras almaktadir.
-
 //? Object Constructor
 function Book(title, author, year) {
     this.title = title;
@@ -36,29 +24,46 @@ function Book(title, author, year) {
   //? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
   //? her bir instance'a da hayat bulmus olur.
   
+  //? instance
   const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
   const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
   
   console.log(book1);
   console.log(book2.getSummary());
-
-  /*
-  const books=[
-    new Book('Kasagi', 'Omer Seyfettin', 1920),
-    new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910)
-  ]*/
   
-
-  book1.price=100;
-  console.log(book1);
-
-
-//! Eğer yeni bir property veya fonksiyonu constructor'a eklemek istersek
-//! prototype'ı kullanabilir
-
-Book.prototype.getAge=function(){
-    return new Date().getFullYear()-this.year
-}
-
-console.log(book1.getAge());
-console.log(book2.getAge());
+  book1.price = 100;
+  console.log(book1, book2);
+  
+  //! Eger yeni bir property veya fonksiyonu constructor'a eklemek istersek
+  //! prototype'ı kullanabiliriz.
+  
+  Book.prototype.getAge = function () {
+    return new Date().getFullYear() - this.year;
+  };
+  
+  Book.prototype.type = 'novel';
+  
+  console.log(book1.getAge());
+  console.log(book2.getAge());
+  console.log(book2.type);
+  
+  
+  //! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
+  //! sekilde metotlar tanimlamaya izin vermektedir.
+  //! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
+  //! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
+  //! ulasabilmesidir.
+  
+  //* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+  //* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
+  
+  //* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+  //* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+  
+  
+  
+  //!Object'lerin proto alanini gormk icin .protoype
+  console.log(Book.prototype);
+  
+  //!instance'larin proto alanini gormk icin .__proto__
+  console.log(book1.__proto__);
