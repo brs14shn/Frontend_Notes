aa357e7cff7bff8ebd74e48f8dfb455ea1960c5e
brs14shn <brsshn292929@gmail.com>
2022-06-18 13:29:06 +0300

    upt

---
+++ javascript/class-notes/js18_oop1/3-classses.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+class Book{
+    constructor(title,author,year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getSummary = function () {
+      return `${this.title} was written by ${this.author} in ${this.year} `;
+    };
+
+    }
+}
