b0b9becc52cbab3740454e49638729b01928447f
brs14shn <brsshn292929@gmail.com>
2022-06-20 20:01:14 +0300

    polymorphism added

--- javascript/class-notes/js18_oop1/2-ObjectConstructor.js
+++ javascript/class-notes/js18_oop1/2-ObjectConstructor.js
@@ -10,81 +10,82 @@
 
 //? Object Constructor
 function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.getSummary = function () {
-      return `${this.title} was written by ${this.author} in ${this.year} `;
-    };
-  }
-  
-  //? new keyword'u Book Constructor'ini parametereler ile cagirmaktadir.
-  //? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
-  //? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
-  //? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
-  //? her bir instance'a da hayat bulmus olur.
-  
-  //? instance
-  Book.prototype.price=100;
-  const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
-  const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
-  
-  
-  //console.log(book1);
-  //console.log(book2);
-  //console.log(book2.getSummary());
-  
- 
-  //console.log(book1, book2);
-  
-  //! Eger yeni bir property veya fonksiyonu constructor'a eklemek istersek
-  //! prototype'ı kullanabiliriz.
-  
-  Book.prototype.getAge = function () {
-    return new Date().getFullYear() - this.year;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} in ${this.year} `;
   };
-  
-  Book.prototype.type = 'novel';
-  
-  //console.log(book1.getAge());
-  //console.log(book2.getAge());
-  //console.log(book2.type);
-  
-  
-  //! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
-  //! sekilde metotlar tanimlamaya izin vermektedir.
-  //! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
-  //! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
-  //! ulasabilmesidir.
-  
-  //* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
-  //* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
-  
-  //* Bir nesnenin prototiplerine .prototype ile erisilebilir.
-  //* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
-  
-  
-  
-  //!Object'lerin proto alanini gormk icin .protoype
-  //console.log(Book.prototype);
-  
-  //!instance'larin proto alanini gormk icin .__proto__
-  //console.log(book1.__proto__);
+}
 
+//? new keyword'u Book Constructor'ini parametereler ile cagirmaktadir.
+//? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
+//? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
+//? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
+//? her bir instance'a da hayat bulmus olur.
 
-  //*========================İNHERİTANCE(KALITIM-es5)===============*
+//? instance
+const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
+const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
 
-  //* CLASSA VERİLEN PROTOTYPELAR DİĞERLERİNE DE GEÇER...
- function Magazine(title,author,year,month){
-    //* Booktan türetilmiş magazine classı almış olduk
-    /* parent'ın object constructorının çağrılması */
-    Book.call(this,title,author,year)
-    this.month=month;
- }
+console.log(book1);
+console.log(book2.getSummary());
 
- //! prototipler doğrudan miras olarak gelmez...eksikkk
- Magazine.prototype=Object.create(Book.prototype)
- const mag1=new Magazine("Scientific Research","Einstein",1905,"September")
- console.log(mag1.getAge());
-//! 
+book1.price = 100;
+console.log(book1, book2);
+
+//! Eger yeni bir property veya fonksiyonu constructor'a eklemek istersek
+//! prototype'ı kullanabiliriz.
+
+Book.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
+
+Book.prototype.type = 'novel';
+
+console.log(book1.getAge());
+console.log(book2.getAge());
+console.log(book2.type);
+
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina
+//! kolay bir sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un
+//! olusan tum instance'lerin belleginde yer kaplamamasi ancak tum
+//! instance'larin bu metota ulasabilmesidir.
+
+//* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+//* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+
+//!Object'lerin proto alanini gormk icin .protoype
+console.log(Book.prototype);
+
+//!instance'larin proto alanini gormk icin .__proto__
+console.log(book1.__proto__);
+
+//? INHERITANCE (Kalitim - ES5)
+//?-------------------------------------------------------------------
+
+//? Sub-Class
+function Magazine(title, author, year, month) {
+  //* parent'in object constructorinin cagrilmasi
+  Book.call(this, title, author, year);
+  this.month = month;
+}
+
+//! Prototipler dogrudan miras olarak gelmez.
+console.log(Magazine.prototype);
+
+//! Prototipleri miras almak icin Object.create() metodu kullanabilir.
+Magazine.prototype = Object.create(Book.prototype);
+console.log(Magazine.prototype);
+
+//* Magazine nesnesinden bir instance olsuturulmasi
+const mag1 = new Magazine('Scientific Research', 'Einstein', 1905, 'September');
+
+console.log(mag1);
+console.log(mag1.getAge());
+
  
--- javascript/class-notes/js18_oop1/3-classses.js
+++ javascript/class-notes/js18_oop1/3-classses.js
@@ -1,47 +1,51 @@
 //* ======================================================
-//*     OOP - Inheritance and Polymorphism(ES6)
+//*           OOP - Classes and Inheritance (ES6)
 //* ======================================================
 
-//? Bir parent class'in degisken ve fonksiyonelliği extends keyword'u ile
-//? child class'a gecmektedir.(INHERITANCE)
+//? Classes'lar, object (nesne) olsuturmak icin kullanilan sablonlardir.
+//? JS'de Class'lar prototipler uzerine insa edilmistir. Ancak, syntax
+//? ES5'den farklidir. Aslinda, class keyword'u ilk olarak ES6 da
+//? kullanilmistir ancak bu sadece bir syntactical sugar'dir.
+//? JavaScript, class-tabanli bir dil degil, prototype-tabanli bir dildir.
+//? yaygin kullanim class-tabanli oldugu icin syntax'ini O'na benzetmistir.
 
-//* Polymorphism, bir degisken, fonksiyon veya nesnenin çoklu sekiller
-//* alabilmesini tanimlayan bir nesne-yonelimli programlama teknigidir.
-//* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar ile bilinir.
+//? Bir parent class'in degisken ve fonksiyonelliği extends
+//? keyword'u ile child class'a gecmektedir.(INHERITANCE)
 
 class Book {
-    constructor(title, author, year) {
-      this.title = title;
-      this.author = author;
-      this.year = year;
-      // this.getTitle = function () {
-      //   return this.title;
-      // };
-    }
-    //! bu kısımda yazılan fonksiyonlar prototype alanında bulunur
-    getSummary() {
-      return `${this.title} was written by ${this.author} in ${this.year} `;
-   }
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    // this.getTitle = function () {
+    //   return this.title;
+    // };
   }
-  
-  //? instance
-  const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
-  const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
-  
-  //console.log(book1);
 
-  //? sub-Class tanımlanması
-  class Magazine extends Book{
-    constructor(title,author,year,month){
-    super(title,author,year);//! bookun protptype copy edildi
-    this.month=month
-    //* super türetilen parent=Book constructor
-
-    }
+  //! Bu kisimda yazilan fonksiyonlar aslinda prototype alaninda bulunur.
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year} `;
   }
-  const mag1=new Magazine("Kaşağı","Ömer Seyfettin",1949,"Nov");
+}
 
-  console.log(mag1);
-  console.log(mag1.getSummary());
+//? instance
+const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
+const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
+
+console.log(book1);
+
+//? Sub-Class tanimlamasi (Inheritance)
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    //! Book'un constructor'i cagrildi
+    super(title, author, year); //! Book'un prototpye kopyalnmis oldu
+    this.month = month;
+  }
+}
+
+const mag1 = new Magazine('Kasagi', 'Omer Seyfettin', 1940, 'Nov');
+console.log(mag1);
+console.log(mag1.getSummary());
+
 
   
