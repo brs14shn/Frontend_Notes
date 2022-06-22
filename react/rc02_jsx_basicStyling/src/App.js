013aea00dd2350933b7a097e34cc3ee92dcd7b49
brs14shn <brsshn292929@gmail.com>
2022-06-22 20:33:56 +0300

    UPDATED

--- react/rc02_jsx_basicStyling/src/App.js
+++ react/rc02_jsx_basicStyling/src/App.js
@@ -1,6 +1,26 @@
+
+//? React,JSX kullanmaktadir.
+//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
+
+
+
+
+
 //? App componentinin tanimlanmasi
 function App() {
   return (
+    //? return içinde JSX kullanılır.COMPONENTLER RETURN İŞLEVİ VARDIR
     <div>
       <h1>APP</h1>
     </div>
--- react/rc02_jsx_basicStyling/src/index.js
+++ react/rc02_jsx_basicStyling/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './App'; //* js yazılması optional css zorunlu ancak
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//! index componenti ReactDOM.createRoot() metodu ile
+//! public klasorunde yer alan index.html icersindeki id'si root
+//! olan elemana erisir.
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //* create ile object haline gelir
 root.render(<App />);
+
+
