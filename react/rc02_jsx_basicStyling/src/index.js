bd63d7b336c0165c5312dc7b0de36f3dc55bd26f
brs14shn <brsshn292929@gmail.com>
2022-06-22 21:01:22 +0300

    v2

--- react/rc02_jsx_basicStyling/src/index.js
+++ react/rc02_jsx_basicStyling/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App'; //* js yazılması optional css zorunlu ancak
-import './index.css';
+import './index.css'; //* doğrudan import yapılır
+
 
 //! index componenti ReactDOM.createRoot() metodu ile
 //! public klasorunde yer alan index.html icersindeki id'si root
