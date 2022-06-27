8752ac06c5be532d7a724db2f4d9b6e12d05dd2d
brs14shn <brsshn292929@gmail.com>
2022-06-27 20:59:02 +0300

    event updated

--- react/rc05_events-useStateHook/src/components/event_intro/Event.jsx
+++ react/rc05_events-useStateHook/src/components/event_intro/Event.jsx
@@ -12,11 +12,56 @@
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Event = () => {
-  return (
-    <div>
-        <button className="btn btn-warning">Click</button>
+
+    let info="EVENTS";
+  
+     const handleClick=()=>{
+        alert("Btn chicked")
+     }
+     const handleClear=(text)=>{
+        alert(text)
+
+     }
+
+     const handleChange=()=>{
+        info="HOOKS"
+        console.log(info)
+        //* Dom basmıyor ancak console da gözüküyor
+        //* React statik olarak görür
+
+     }
+    return (
+    <div className="container text-center mt-4">
+        <div className="display-4 text-danger m-4">{info}</div>
+
+        <button onClick={handleClick} className="btn btn-warning">Click</button>
+        {/* Eger bir event fonksiyonunun paremetresi olmasi gerkiyorsa 
+      bu fonksiyon bir arrow fonks. tarafindan  cagrilmalidir. Aksi 
+      takdirde event fonksiyonu event gerceklesmeden cagirilir */}
+        <button onClick={()=>handleClear("Clear buton pressed")} className="btn btn-dark ms-3">Clear</button>
+
+        <button onClick={handleChange} className="btn btn-primary ms-3">Change</button>
+        
     </div>
   )
 }
 
 export default Event;
+
+
+
+//* arrow fonksiyonu refere ediliyorsa handleClick() || handleClick("Clicked") şeklinde olmalıdır.
+//! info console'da guncellendigini ancak DOM'da guncellenmedigini gorduk.
+//* Bunun sebebi REACT'in aksi belirtilmedigi surece elementleri
+//* static olarak kabul etmesinden kaynaklanir.
+
+//* React bunu DOM islemlerini minimize etmek icin yapmaktadir.
+//* REACT'a hangi elementleri interaktif oldugu belirtilmelidir.
+//! React'a elementlerin interaktif oldugunu belirtmek icin state'ler kullanilir.
+//! State, elementlerin degisiklik durumlarini tutan nesnelerdir.
+//? ReactJs'de state'leri kullanmak icin  2 ayri Component yapisi bulunmaktadir.
+//? 1. Statefull Classes (Class Components).
+//? 2. Hooks (Functional Components).
+
+//?Biz su ana kadar uygulamalarimizda Fonksiyonel Component'leri kullandik.
+//? Yaygin kullanim Fonksiyonel Component'lerdir.
