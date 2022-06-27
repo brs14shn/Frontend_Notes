446c6972b2f36f43bdfd20e2901224fd92402359
brs14shn <brsshn292929@gmail.com>
2022-06-27 20:21:00 +0300

    event

---
+++ react/rc05_events-useStateHook/src/components/event_intro/Event.jsx
@@ -0,0 +1,20 @@
+
+// * =======================================================
+// *                     EVENTS
+// * =======================================================
+
+//? ReactJS, Tarayicilar arasi uyumluluk ve performans artisi gibi
+//? sebeplerden oturu Sentetik Event olarak adilandirilan Olaylari
+//? kullanir. Sentetik Event, aslinda tarayicinin dogal event'larinin
+//? bir sarmalayici (Wrapper) arabirimle ortulmesi ile olusur. Bu sayede,
+//? React ortaminda kullanilan event'larin bilindik tarayicilarda
+//? sorunsuz calismasini saglanir.
+//? Ayrinti icin : https://reactjs.org/docs/events.html
+
+const Event = () => {
+  return (
+    <div>Event</div>
+  )
+}
+
+export default Event;
