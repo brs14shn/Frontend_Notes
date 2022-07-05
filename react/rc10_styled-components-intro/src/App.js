e2cc45ed3f24374bda494973ab5fce0367260dee
brs14shn <brsshn292929@gmail.com>
2022-07-05 20:50:59 +0300

    Container  created

--- react/rc10_styled-components-intro/src/App.js
+++ react/rc10_styled-components-intro/src/App.js
@@ -1,11 +1,16 @@
 import Button, { TomatoButton } from './components/Button.styled';
+import Container from "./components/Container.styled";
 
 const App = () => {
   return (
     <>
+      <Container bg="green">
       <Button primary>Click</Button>
       <Button>Click</Button>
       <TomatoButton primary>Click</TomatoButton>
+      <TomatoButton >Click</TomatoButton>
+      </Container>
+      
     </>
   );
 };
