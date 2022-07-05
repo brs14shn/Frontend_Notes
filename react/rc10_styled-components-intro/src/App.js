928d8a27546ac863b962b8fb8174f512705c5ad4
brs14shn <brsshn292929@gmail.com>
2022-07-05 21:00:45 +0300

    Header and Link created

--- react/rc10_styled-components-intro/src/App.js
+++ react/rc10_styled-components-intro/src/App.js
@@ -1,10 +1,12 @@
 import Button, { TomatoButton } from './components/Button.styled';
 import Container from "./components/Container.styled";
+import HeaderText from './components/Header.styled';
 
 const App = () => {
   return (
     <>
       <Container bg="green">
+       <HeaderText color='white' >STYLED COMPONENTS</HeaderText>
       <Button primary>Click</Button>
       <Button>Click</Button>
       <TomatoButton primary>Click</TomatoButton>
