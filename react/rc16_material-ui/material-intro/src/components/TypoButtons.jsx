3b7a65c5147a1334c956b43c47aa56e9d3a59ab2
brs14shn <brsshn292929@gmail.com>
2022-07-18 20:37:22 +0300

    Typography updated

--- react/rc16_material-ui/material-intro/src/components/TypoButtons.jsx
+++ react/rc16_material-ui/material-intro/src/components/TypoButtons.jsx
@@ -6,9 +6,29 @@ import { Typography } from "@mui/material";
 const TypoButtons = () => {
   return (
     <div>
-      <Typography variant="h5">Typography,Buttons,Container,Box</Typography>
+      <Typography
+        variant="subtitle"
+        component="h1"
+        align="center"
+        color="primary"
+      >
+        Typography,Buttons,Container,Box
+      </Typography>
+      <Typography
+        variant="caption"
+        component="h1"
+        align="center"
+        color="secondary"
+      >
+        Typography,Buttons,Container,Box
+      </Typography>
+      <Typography variant="h6" component="h1" align="justify" color="error">
+        Typography,Buttons,Container,Box
+      </Typography>
     </div>
   );
 };
 
 export default TypoButtons;
+
+//* görüntü olarak h5 ama styled olarak h1 olarak
