5b4601eddf0b680b7db65c1bca82c802ce7c9320
brs14shn <brsshn292929@gmail.com>
2022-07-23 11:23:42 +0300

    dependecies

--- react/rc21_redux-toolkit-example/src/components/Navbar.jsx
+++ react/rc21_redux-toolkit-example/src/components/Navbar.jsx
@@ -4,13 +4,20 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            onClick={() => navigate("/login")}
+          >
             Clarusway News
           </Typography>
 
