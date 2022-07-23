ae614a2b9244c7b5844c3c68b472a3138ed2ac15
brs14shn <brsshn292929@gmail.com>
2022-07-23 11:51:55 +0300

    authSlice updated

--- react/rc21_redux-toolkit-example/src/features/authSlice.jsx
+++ react/rc21_redux-toolkit-example/src/features/authSlice.jsx
@@ -1,1 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initilaState = {
+  user: "",
+};
+
+//? createSlice, Redux state lojiğini kisa yoldan tanimlamak icin kullanilan bir metotdur.
+//? slice ismi, state'lerin baslangic degerleri ve reducer'lar key-value yapisi seklinde tanimlanir.
+//? reducer, state'i degistiren fonksiyonlarin yaninda otomatik olarak action type'larin tanimlanmasini da saglar.
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initilaState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = "";
+    },
+  },
+});
