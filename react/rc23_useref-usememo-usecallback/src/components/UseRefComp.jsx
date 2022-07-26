11156f6bcc2f660e12bfbb127a7d24dbca71dac7
brs14shn <brsshn292929@gmail.com>
2022-07-26 20:57:18 +0300

    useRef started

---
+++ react/rc23_useref-usememo-usecallback/src/components/UseRefComp.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+
+const UseRefComp = () => {
+  //* useRef 1. kullanım--
+  //* 👇 DOM elementlerine ulaşmak için kullanılır
+
+  const inputRef = useRef(null);
+
+  const divRef = useRef();
+  useEffect(() => {
+    // document.querySelector("input").focus()
+    // console.log(inputRef.current.focus());
+  }, []);
+
+  const changeBgColor = () => {
+    //! RANDOM COLOR
+
+    // const colors = [
+    //   "orange",
+    //   "pink",
+    //   "purple",
+    //   "red",
+    //   "#00FF00",
+    //   "rgb(150, 150, 150)",
+    // ];
+    // const random = Math.trunc(Math.random() * colors.length);
+    // console.log(random);
+    //? div elemetini yakaladık.
+    // divRef.current.style.backgroundColor = colors[random];
+    divRef.current.style.backgroundColor = inputRef.current.value;
+  };
+
+  return (
+    <div ref={divRef} className="useref">
+      <h2>UseRefComponents</h2>
+      <input ref={inputRef} type="text" placeholder="Enter text..." />
+      <input
+        style={{ marginTop: "1rem" }}
+        ref={inputRef}
+        type="color"
+        placeholder="Enter text..."
+      />
+      <button onClick={changeBgColor}>ChangeBgColor</button>
+    </div>
+  );
+};
+
+export default UseRefComp;
