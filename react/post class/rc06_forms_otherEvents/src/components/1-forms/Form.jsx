daf6dc4aa2ddf98ec2b27bf722d3be123cc5504f
brs14shn <brsshn292929@gmail.com>
2022-10-07 23:24:57 +0300

    form added

--- react/post class/rc06_forms_otherEvents/src/components/1-forms/Form.jsx
+++ react/post class/rc06_forms_otherEvents/src/components/1-forms/Form.jsx
@@ -1,9 +1,73 @@
 
+import {useState} from "react"
+
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+const [username, setUsername] = useState("")
+const [email, setEmail] = useState("")
+const [password,setPassword] =useState("")
+
+const handleUsername =(e)=>{
+  //console.log(e.target.value)
+ setUsername(e.target.value)
 }
 
-export default Form
+const handleSubmit =(e)=>{
+  e.preventDefault()  //* Refresh özelliğini kapattık.
+  alert(`username  : ${username}`)
+  // setEmail("")
+  // setPassword("")
+  // setUsername("")
+  
+}
+
+  return (
+    <div  >
+      <form onSubmit={handleSubmit} style={{margin:"5rem"}}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username :   <span className="text-danger">{username}</span>
+          </label>
+          <input
+            onChange={handleUsername}
+            type="text"
+            className="form-control"
+            id="username"
+            // value={username}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address : {email}
+          </label>
+          <input
+            onChange={(e)=>setEmail(e.target.value)}
+            type="email"
+            className="form-control"
+            id="email"
+            // value={email}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            onChange={(e)=>setPassword(e.target.value)}
+            type="password"
+            className="form-control"
+            id="password"
+            // value={password}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
