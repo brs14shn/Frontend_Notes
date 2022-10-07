b30f53b6e8f52e68d2465e0ab7a329c513eb80a9
brs14shn <brsshn292929@gmail.com>
2022-10-08 01:27:41 +0300

    FormObjects

--- react/post class/rc06_forms_otherEvents/src/App.js
+++ react/post class/rc06_forms_otherEvents/src/App.js
@@ -1,4 +1,5 @@
 import Form from './components/1-forms/Form';
+import FormObject from './components/1-forms/FormObject';
 import KeyboardEvents from './components/2-keyboardClipboardEvents/KeyboardClipboard';
 import MouseEvent from './components/3-mouseEvents/MouseEvent';
 
@@ -8,7 +9,8 @@ function App() {
  
   return (
     <div className="container mt-4 text-center">
-      <Form />
+      {/* <Form /> */}
+      <FormObject/>
       {/* <KeyboardEvents />
       <MouseEvent /> */}
     </div>
