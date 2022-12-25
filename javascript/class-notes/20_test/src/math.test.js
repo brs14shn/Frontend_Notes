b4943b48c18ca0fcbbbaf0459db7a728a7a1002c
brs14shn <brsshn292929@gmail.com>
2022-12-25 14:41:41 +0300

    testing-jest

---
+++ javascript/class-notes/20_test/src/math.test.js
@@ -0,0 +1,42 @@
+//! node module import synax
+
+const {
+    calculateTotal,
+    celsiusToFahrenheit,
+    fahrenheitTocelsius,
+    fibonacci} = require("./math");
+
+
+//! es6 module export
+// import{
+//     calculateTotal,
+//     fahrenheitTocelsius,
+//     celsiusToFahrenheit,
+//     fibonacci
+
+describe("calculateTotal",()=>{
+   
+    // it("should exist",()=>{
+    //     expect(calculateTotal).toBeDefined();
+    // })
+    // test("calculates total with default vat",()=>{
+    //     expect(calculateTotal(100)).toBe(118);
+    // });
+    // test("calculates total with 0 vat",()=>{
+    //     expect(calculateTotal(100,0)).toBe(100);
+    // });
+    // test("calculates total with 0.1 vat",()=>{
+    //     expect(calculateTotal(100,0.1)).toBe(110);
+    // });
+})
+
+describe("celsiusToFahrenheit",()=>{
+    
+})
+describe("fahrenheitTocelsius",()=>{
+    
+})
+
+describe("fibonacci",()=>{
+
+})
